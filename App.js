861234d8a38533fa3c4fc0a3f4661bf4efadf738
KarlStevenRequinto <lerdrobott@gmail.com>
2023-04-04 10:12:23 +0800

    text input component done

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import CustomButton from './src/components/CustomButton';
+import CustomTextInput from './src/components/CustomTextInput';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <CustomButton btnTitle="Login"/>
+      <CustomTextInput placeholder="Enter your email"/>
       <StatusBar style="auto" />
     </View>
   );
--- src/components/CustomTextInput/index.js
+++ src/components/CustomTextInput/index.js
@@ -1,14 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import React from "react";
 
-const CustomTextInput = () => {
+const CustomTextInput = ({
+  placeholder,
+  iconPlaceholder,
+  textInputContainerStyle,
+}) => {
   return (
-    <View>
-      <Text>CustomTextInput</Text>
+    <View style={[styles.container, textInputContainerStyle]}>
+      <TextInput placeholder={placeholder} placeholderTextColor={"#67686D"} />
+      <View style={styles.iconContainer}>{iconPlaceholder}</View>
     </View>
-  )
-}
+  );
+};
 
-export default CustomTextInput
+export default CustomTextInput;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#3A3F47",
+    justifyContent: "center",
+    padding: 12,
+    borderRadius:8,
+    flexDirection: "row",
+  },
+  iconContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
